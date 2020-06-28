f06c91862b9235cc4a0a8942f050d04ddf8ef6aa
Guo <564559079@qq.com>
2020-06-28 19:55:56 +0800

    添加代码生成模块

---
+++ lucky-shop-generator/src/main/java/com/lucky/shop/generator/LuckyShopGeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.lucky.shop.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Admin
+ */
+@SpringBootApplication
+public class LuckyShopGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyShopGeneratorApplication.class, args);
+    }
+
+}
---
+++ lucky-shop-generator/src/test/java/com/lucky/shop/generator/LuckyShopGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.shop.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyShopGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
